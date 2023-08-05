4fe20bc90b7ab0cb10310428bb29e3f63799beb0
VaibhavTiwariii <138752219+VaibhavTiwariii@users.noreply.github.com>
2023-08-05 15:37:36 +0530

    portfolio-resume committed

---
+++ app.js
@@ -0,0 +1,8 @@
+// var typed = new typed('#element', {
+//     strings: ['Expert in HTML/CSS', ' JavaScript-jquery','MERN Stack'],
+//     typeSpeed: 50,
+//   });
+var typed = new typed('#element', {
+  strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+  typeSpeed: 50,
+});
